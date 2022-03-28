67cf88c6e9912752446fb7d2ea9cc4ea0a7227a3
david <55272383+cioatodavid@users.noreply.github.com>
2022-03-28 20:08:08 -0300

    Update ano bissexto.py

--- ano bissexto.py
+++ ano bissexto.py
@@ -1,10 +1,8 @@
-import math
-
 print('Digite um ano para saber se ele é bissexto: ')
 year = int(input())
-if math.remainder(year,4) == 0:
-    if math.remainder(year,100) == 0:
-        if math.remainder(year,400) == 0:
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
               print('É um ano bissexto')
         else:
             print('Não é um bissexto')   
