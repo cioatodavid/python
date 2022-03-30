90b0777f83d571fc24e52dfe13f7a996cbb081a7
david <55272383+cioatodavid@users.noreply.github.com>
2022-03-30 19:21:27 -0300

    Update exec1049.py

--- beecrowd/exec1049.py
+++ beecrowd/exec1049.py
@@ -1,5 +1,29 @@
-dict = {
-    'vertebrado': 'ave'
-    
-    
-}
+x = input()
+if x == "vertebrado":
+    y = input()
+    if y == 'mamifero':
+        z = input()
+        if z == 'onivoro':
+            print('homem')
+        elif z == 'herbivoro':
+            print('vaca')
+    if y == 'ave':
+        z = input()
+        if z == 'onivoro':
+            print('pomba')
+        elif z == 'carnivoro':
+            print('aguia')
+if x == 'invertebrado':
+    y = input()
+    if y == 'inseto':
+        z = input()
+        if z == 'hematofago':
+            print('pulga')
+        elif z == 'herbivoro':
+            print('lagarta')
+    if y == 'anelideo':
+        z = input()
+        if z == 'hematofago':
+            print('sanguessuga')
+        elif z == 'onivoro':
+            print('minhoca')
